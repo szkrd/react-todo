4224fe86333990a5bf54ec5b22814756df5c7228
szkrd <szabolcs.kurdi@gmail.com>
2017-07-13 11:45:08 +0200

    test(app): jest basics

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme';
 import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+describe('App', () => {
+  beforeEach(() => {
+    global.localStorage = {
+      getItem: jest.fn(), // sinon not really needed, jest has a builtin mock
+      setItem: jest.fn()
+    }
+  })
+
+  afterEach(() => {
+    delete global.localStorage
+  })
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<App />, div)
+  })
+
+  describe.only('onAdd', () => {
+    it('should add a todo item', () => {
+      const app = shallow(<App/>)
+      const instance = app.instance();
+      instance.saveState = jest.fn() // jest is already available
+      instance.onAdd({
+        text: 'foo',
+        priority: 1
+      })
+      expect(instance.state).toEqual({
+        addComponentVisible: false,
+        todos: [ { text: 'foo', priority: 1, id: 1 } ]
+      })
+      expect(instance.saveState).toHaveBeenCalled()
+    })
+  })
 })
+
